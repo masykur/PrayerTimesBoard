bf8ba3070926e21a99d40bfd5a42ea1202eaf134
Ahmad Masykur <cahnom@gmail.com>
2022-10-02 13:31:29 +0700

    Add javanese calendar

--- ui/src/Calendar.js
+++ ui/src/Calendar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { format, add, formatDistanceStrict } from 'date-fns'
+import { format, add } from 'date-fns'
 import { id } from 'date-fns/locale'
 import { Coordinates, CalculationMethod, PrayerTimes } from 'adhan';
 import './Calendar.css'
@@ -7,7 +7,7 @@ import './Calendar.css'
 class Calendar extends React.Component {
     constructor(props) {
       super(props);
-        this.state = {now: new Date(), nextPrayer: 'none', nextPrayerTime: new Date()};
+        this.state = {now: new Date(), nextPrayer: 'none', nextPrayerTime: new Date(), javaDate: { day: 1, month: 1, year: 1, monthName: ""}};
     }
     
     componentDidMount() {
@@ -35,10 +35,12 @@ class Calendar extends React.Component {
         } else {
             var nextPrayerTime = prayerTimes.timeForPrayer(next);
         }
+        var jd = this.javaDate(now)
         this.setState({
-          now: new Date(),
+          now: now,
           nextPrayer: next,
-          nextPrayerTime: nextPrayerTime
+          nextPrayerTime: nextPrayerTime,
+          javaDate: jd
         });
     }
 
@@ -54,21 +56,85 @@ class Calendar extends React.Component {
       }
       return { fajr: "Subuh", sunrise: "Suruq", dhuhr: "Dhuhur", asr: "Asr", maghrib: "Maghrib", isha: "Isha" }[name];
     }
-    pasaran(date) {
+    javaDayOfWeek(date) {
       var index = Math.floor((date.getTime()) / 86400000) % 5;
-      //var index = Math.floor((dateOnly - new Date(1970, 1, 1)) / 1000 / 60 / 60 / 24) % 5;
       return [ "Wage", "Kliwon", "Legi", "Pahing", "Pon"][index];
     }
+    javaDate(date) {      
+      const monthNames = ["Suro", "Sapar", "Mulud", "Bakda Mulud", "Jumadil Awal", "Jumadil Akhir", "Rejeb", "Ruwah", "Poso", "Sawal", "Sela", "Besar"];
+      const javaDayNames = [ "Kliwon", "Legi", "Pahing", "Pon", "Wage"];
+      const dayNames = [ "Ahad", "Senin", "Selasa", "Rabu", "Kamis", "Jumu'ah", "Sabtu"];
+      const firstSuro = [ 
+        { Day : "Selasa", JavaDay : "Pon" },
+        { Day : "Sabtu", JavaDay : "Pahing" },
+        { Day : "Kamis", JavaDay : "Paing" },
+        { Day : "Senin", JavaDay : "Legi" },
+        { Day : "Jumu'ah", JavaDay : "Kliwon" },
+        { Day : "Rabu", JavaDay : "Kliwon" },
+        { Day : "Ahad", JavaDay : "Wage" },
+        { Day : "Kamis", JavaDay : "Pon" },
+      ];
+      const daysAge =  
+      [
+        [0, 29, 30, 29, 30, 29, 30, 29, 30, 29, 30, 29], // alip
+        [30, 29, 30, 29, 30, 29, 30, 29, 30, 29, 30, 30], // ehe (kabisat)
+        [30, 29, 30, 29, 30, 29, 30, 29, 30, 29, 30, 29], // jumawal
+        [30, 29, 30, 29, 30, 29, 30, 29, 30, 29, 30, 30], // je (kabisat)
+        [30, 30, 30, 29, 29, 29, 30, 29, 30, 29, 30, 29], // dal
+        [30, 29, 30, 29, 30, 29, 30, 29, 30, 29, 30, 29], // be
+        [30, 29, 30, 29, 30, 29, 30, 29, 30, 29, 30, 29], // wawu
+        [30, 29, 30, 29, 30, 29, 30, 29, 30, 29, 30, 30]  // jimakir (kabisat)
+      ];
+      var days = 122903 + Math.floor(date.getTime() / 86400000);
+      var year = Math.floor(days / 354.375);
+      var windu = year % 8;
+      var deltaDay = Math.floor(days - (year * 354.375));
+      var firstSuroDay = Math.floor(date.getTime() / 86400000) + (-1 * deltaDay);
+      // var firstSuroDate = new Date(firstSuroDay * 86400000);
+      // var firstSuroDayOfWeek = (firstSuroDay + 5) % 7;
+      // var firstSuroJavaDayOfWeek = (firstSuroDay + 5) % 5;
+      var dayOfFirstSuro = firstSuro[windu];
+      var firstSuroDayAdjustment = firstSuroDay;
+      var i=0;
+      while(dayNames[(firstSuroDayAdjustment + 5) % 7] !== dayOfFirstSuro.Day && javaDayNames[(firstSuroDayAdjustment + 5) % 5] !== dayOfFirstSuro.JavaDay) {
+        i++;
+        firstSuroDayAdjustment = firstSuroDay - i;
+        if (dayNames[(firstSuroDayAdjustment + 5) % 7] === dayOfFirstSuro.Day && javaDayNames[(firstSuroDayAdjustment + 5) % 5] === dayOfFirstSuro.JavaDay) {
+          break;
+        }
+        firstSuroDayAdjustment = firstSuroDay + i;
+      }
+      var newYear = firstSuroDayAdjustment;
+      var daysLeft = (Math.floor(date.getTime() / 86400000) - newYear) + 1;
+      var month = 0;
+      while (daysLeft > daysAge[windu][month])
+      {
+        daysLeft -= daysAge[windu][month];
+        month++;
+      }
+      console.info("days", new Date((days-122903) * 86400000));
+      return { 
+        year: year + 1555, 
+        month: month + 1, 
+        monthName: monthNames[month], 
+        day : daysLeft,
+        dayOfWeek: ((days-122903)+4) % 7,
+        dayOfWeekName: dayNames[((days-122903)+4) % 7],
+        javaDayOfWeek: ((days-122903)+4) % 5,
+        javaDayOfWeekName: javaDayNames[((days-122903)+4) % 5]
+       };
+    }
+
     render() {
       return (
         <div className="calendar">
           <div>
-          <div>{format(this.state.now, "EEEE", { locale: id })}</div>
-          <div>{this.pasaran(this.state.now)}</div>
+          <div>{this.state.javaDate.dayOfWeekName}</div>
+          <div>{this.state.javaDate.javaDayOfWeekName}</div>
           </div>
           <div>
             <div>{format(this.state.now, "d MMMM yyyy")}</div>
-            <div></div>
+            <div>{this.state.javaDate.day} {this.state.javaDate.monthName} {this.state.javaDate.year}</div>
           </div>
           <div className="clock">
               <h2>{format(this.state.now, "HH:mm")}</h2>
